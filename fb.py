664c8fbb2df5d3feb364ca175ab6e91fe4e6dbdb
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-24 01:41:12 -0400

    made changes to better suit pyboy

--- fb.py
+++ fb.py
@@ -11,24 +11,18 @@ class RenderEngine:
         self.SYS_VIDEO_BUFFER = open("/dev/fb0","r+b")
         self.LOCAL_BUFFER = self.SYS_VIDEO_BUFFER.read()
         self.LOCAL_QUEUE = {}
-        self.COLOURS = {"RED":b'\x00\x00\xFF\x00',"GREEN":b'\x00\xFF\x00\x00',"BLUE":b'\xFF\x00\x00\x00',"PURPLE":b'\xFF\x00\xFF\x00',"WHITE":b'\xFF\xFF\xFF\x00',"BLACK":b'\x00\x00\x00\x00',"YELLOW":b'\x00\xFF\xFF\x00',"PASTEL_PINK":b'\xFC\xCF\xF6\x00',}
+
+        self.CURRENT_FRAME = {}
+        self.FRAME_HEIGHT = 0
+        self.FRAME_WIDTH = 0
+        self.CURRENT_PIXELS = {}
     
-    def getPosition(self, x: int, y: int):
+    def getPosition(self, x:int, y:int) -> int:
         return (y * self.WIDTH + x) * self.BYTES_PER_PIXEL
-    
-    def writePixel(self, x: int, y: int, Bytes: bytes):
-        if 0 <= x < self.WIDTH and 0 <= y < self.HEIGHT:
-            position = self.getPosition(x,y)
-            self.SYS_VIDEO_BUFFER.seek(position)
-            self.SYS_VIDEO_BUFFER.write(Bytes)
-    
-    def writeToBuffer(self, Bytes: bytes, position: int = 0) -> None:
-        self.SYS_VIDEO_BUFFER.seek(position)
-        self.SYS_VIDEO_BUFFER.write(Bytes)
 
     def queueLocalChange(self, x: int, y: int, Bytes: bytes) -> None:
         if 0 <= x < self.WIDTH and 0 <= y < self.HEIGHT:
-            position = self.getPosition(x,y)
+            position = self.getPosition()
             self.LOCAL_QUEUE[position] = Bytes
 
     def updateLocalBuffer(self) -> None:
@@ -57,49 +51,29 @@ class RenderEngine:
         self.clearFrameBuffer()
         print("\n"*(self.HEIGHT//14))
 
-    def drawRectangle(self, start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes) -> None:
-        for j in range(start_y, end_y):
-            for i in range(start_x, end_x):
-                self.queueLocalChange(i, j, colour)
+    def setupParams(self, frame: Image):
+        width, height = frame.size()
+        self.FRAME_WIDTH, self.FRAME_HEIGHT = width, height
+        self.convertFrame(frame)
+        self.CURRENT_PIXELS = dict(self.CURRENT_FRAME)
 
-    def drawLine(self, start_x: int, start_y: int, end_x: int, end_y: int, colour: bytes, thickness: int = 3) -> None:
-        slope = (end_y - start_y) / (end_x - start_x)
-        c = start_y - slope * start_x
-        for x in range(start_x, end_x + 1):
-            for i in range(thickness+1):
-                y = round(slope * (x+i) + c)
-                self.queueLocalChange(x, y, colour)
-                self.queueLocalChange(x, y - (i * 2), colour)
+    def convertRGBtoBGRA(self, r:int, g:int, b:int) -> bytes:
+        r = r << (16)
+        g = g << (8)
+        return (r + g + b).to_bytes(4,'little')
 
-    def drawCircle(self, center_x: int, center_y: int, radius: int, colour: bytes, thickness: int = 3) -> None:
-        e = -radius
-        y = 0
-        while y < radius:
-            for i in range(thickness):
-                # TODO: Streamline these for loops, mostly finished but I feel there can be more
-                for x_sign, y_sign in [[1,1],[1,-1],[-1,1],[-1,-1]]:
-                    [self.queueLocalChange(center_x + (radius*x_sign), center_y + (y*y_sign) + (i*o), colour) for o in [1,-1]]
-                for y_sign, x_sign in [[1,1],[1,-1],[-1,1],[-1,-1]]:
-                    [self.queueLocalChange(center_x + (y*y_sign), center_y + (radius*x_sign) + (i*o), colour) for o in [1,-1]]
-            e+=2*y+1
-            y+=1
-            if e >= 0:
-                e-=2*radius-1
-                radius-=1
+    def convertFrame(self, frame: Image):
+        px = frame.load()
+        for y in range(self.FRAME_HEIGHT):
+            for x in range(self.FRAME_WIDTH):
+                self.CURRENT_FRAME[self.getPosition(x,y)] = self.convertRGBtoBGRA(*px[x,y])
+        frame.close()
 
-    def drawImage(self, filePath:str, start_x:int =0, start_y:int =0) -> None:
-        if filePath.endswith(".zeke"):
-            try:
-                image = open(filePath, "rb")
-                data = bytearray(image.read())
-                width = int.from_bytes(data[0:2],'little')
-                height = int.from_bytes(data[2:4],'little')
-                cursor = 4
-                for y in range(height):
-                    for x in range(width):
-                        self.queueLocalChange(x+start_x,y+start_y,data[cursor:cursor+3]+b'\x00')
-                        cursor+=3
-            except FileNotFoundError as e:
-                raise e
-        else:
-            raise TypeError("File format not supported")
+    def drawFrame(self, frame: Image) -> None:
+        self.convertFrame(frame)
+        cursor = 4
+        for y in range(self.FRAME_HEIGHT):
+            for x in range(self.FRAME_WIDTH):
+                if self.CURRENT_PIXELS[self.getPosition(x,y)] != self.CURRENT_FRAME[self.getPosition(x,y)]:
+                    self.queueLocalChange(x,y,self.CURRENT_FRAME)
+                    cursor+=3
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from pyboy import PyBoy
+from pyboy import PyBoy, WindowEvent, logger
+from fb import RenderEngine
+from PIL import Image
 
-pyboy = PyBoy('game.gb')
-while not pyboy.tick():
-    pyboy.screen_image()
-    input()
+logger.log_level("DISABLE")
 
-pyboy.stop()
+LAST_FRAME = bytearray(bytes(0))
+
+def main():
+    screen = RenderEngine()
+    pyboy = PyBoy('game.gb', window_type="headless")
+    pyboy.tick()
+    screen.setupParams(pyboy.screen_image())
+    while not pyboy.tick():
+        screen.drawFrame(pyboy.screen_image())
+        input()
+    pyboy.stop()
+
+if __name__ == "__main__":
+    main()
