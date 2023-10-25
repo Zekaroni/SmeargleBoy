376e47037b10783d8385e13660660d8aef2a7733
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-10-25 19:49:44 -0400

    chatGPT frame counter

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from fb import RenderEngine
 from pyboy import PyBoy, WindowEvent, logger
+import time
 
 logger.log_level("DISABLE")
-
-LAST_FRAME = bytearray(bytes(0))
 
 def main():
     screen = RenderEngine()
     pyboy = PyBoy('game.gb', window_type="headless")
     pyboy.tick()
     screen.setupParams(pyboy.screen_image())
-    
+
+    frame_count = 0
+    start_time = time.time()
+
     while not pyboy.tick():
         screen.drawFrame(pyboy.screen_image())
         screen.updateFrameBuffer()
+        frame_count += 1
+        if time.time() - start_time >= 1.0:
+            fps = frame_count / (time.time() - start_time)
+            print(f"FPS: {fps:.2f}\r", end="")
+            frame_count = 0
+            start_time = time.time()
+
     pyboy.stop()
 
 if __name__ == "__main__":
